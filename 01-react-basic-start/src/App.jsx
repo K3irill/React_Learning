012dd3613ee915d3b011a82bf6fd6dc633887302
Kirill <razdor.2019@mail.ru>
2024-11-05 23:37:55 +0300

    refactor

--- 01-react-basic-start/src/App.jsx
+++ 01-react-basic-start/src/App.jsx
@@ -1,42 +1,39 @@
-import Header from "./components/Header/Header";
-import TeachingSection from "./components/TeachingSeaction";
-import DifferencesSection from "./components/Differences";
-import IntroSection from "./components/IntroSection";
-import TabsSection from "./components/TabsSection";
-import FeedbackSection from "./components/FeedbackSection";
-import { useState } from "react";
-import EffectsSection from "./components/EffectsSection";
+import Header from './components/Header/Header'
+import TeachingSection from './components/TeachingSeaction'
+import DifferencesSection from './components/Differences'
+import IntroSection from './components/IntroSection'
+import TabsSection from './components/TabsSection'
+import FeedbackSection from './components/FeedbackSection'
+import { useState } from 'react'
+import EffectsSection from './components/EffectsSection'
 export default function App() {
-  const [visible, setVisible] = useState(true);
-  const [tab, setTab] = useState("effects");
+	const [visible, setVisible] = useState(true)
+	const [tab, setTab] = useState('effects')
 
-  // setTimeout(() => {
-  //   setVisible(false);
-  // }, 3000);
-  return (
-    <>
-      {visible && <Header />}
-      <main>
-        <IntroSection />
-        <TabsSection active={tab} onChange={(current) => setTab(current)} />
+	return (
+		<>
+			{visible && <Header />}
+			<main>
+				<IntroSection />
+				<TabsSection active={tab} onChange={current => setTab(current)} />
 
-        {tab === "main" && (
-          <>
-            <TeachingSection />
-            <DifferencesSection />
-          </>
-        )}
-        {tab === "feedback" && (
-          <>
-            <FeedbackSection />
-          </>
-        )}
-        {tab === "effects" && (
-          <>
-            <EffectsSection />
-          </>
-        )}
-      </main>
-    </>
-  );
+				{tab === 'main' && (
+					<>
+						<TeachingSection />
+						<DifferencesSection />
+					</>
+				)}
+				{tab === 'feedback' && (
+					<>
+						<FeedbackSection />
+					</>
+				)}
+				{tab === 'effects' && (
+					<>
+						<EffectsSection />
+					</>
+				)}
+			</main>
+		</>
+	)
 }
