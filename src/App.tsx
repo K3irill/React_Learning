d5bb9a57e1be6d5bf89c00de6ea75d94a58d4043
Kirill <razdor.2019@mail.ru>
2024-11-17 13:57:56 +0300

    basic routing

--- src/App.tsx
+++ src/App.tsx
@@ -1,13 +1,14 @@
 import React, { useContext } from 'react'
+import { BrowserRouter as Router, Route, Routes, Link } from 'react-router-dom'
 import Greeting from './components/Greeting'
-import AboutUseState from './hooks/about.useState'
-import AboutUseEffect from './hooks/about.useEffect'
+import AboutUseState from './about-hooks/about.useState'
+import AboutUseEffect from './about-hooks/about.useEffect'
 import ControlledForm from './forms/controlledForm'
 import TodoList from './forms/todoList'
-import AboutUseRef from './hooks/about.useRef'
-import AboutUseMemo from './hooks/about.useMemo'
-import AboutUseCallback from './hooks/about.useCallback'
-import { ThemeContext } from './hooks/about.useContext'
+import AboutUseRef from './about-hooks/about.useRef'
+import AboutUseMemo from './about-hooks/about.useMemo'
+import AboutUseCallback from './about-hooks/about.useCallback'
+import { ThemeContext } from './about-hooks/about.useContext'
 
 import './App.css'
 interface user {
@@ -30,28 +31,72 @@ const App: React.FC = () => {
 		hobby: ['Programming', 'English', 'Self-development', 'home sports'],
 	}
 	return (
-		<div
-			style={{
-				backgroundColor: theme === 'light' ? '#fff' : '#242424',
-				color: theme === 'light' ? '#242424' : '#fff',
-			}}
-		>
-			<h1>Application</h1>
-			<button onClick={toggleTheme}>Toggle Theme</button>
-			<Greeting name={user.name} age={user.age} hobby={user.hobby} />
-			<AboutUseState />
-			<AboutUseEffect />
-			<br />
-			<ControlledForm />
-			<br />
-			<TodoList></TodoList>
-			<br />
-			<AboutUseRef></AboutUseRef>
-			<br />
-			<AboutUseMemo />
-			<br />
-			<AboutUseCallback />
-		</div>
+		<Router>
+			<div
+				style={{
+					backgroundColor: theme === 'light' ? '#fff' : '#242424',
+					color: theme === 'light' ? '#242424' : '#fff',
+				}}
+			>
+				<h1>Application</h1>
+				<nav>
+					<ul
+						style={{
+							display: 'flex',
+							listStyle: 'none',
+							gap: '15px',
+							alignItems: 'center',
+							justifyContent: 'center',
+						}}
+					>
+						<li>
+							<Link to='/'>Home</Link>
+						</li>
+						<li>
+							<Link to='/about-use-state'>useState</Link>
+						</li>
+						<li>
+							<Link to='/about-use-effect'>useEffect</Link>
+						</li>
+						<li>
+							<Link to='/about-use-ref'>useRef</Link>
+						</li>
+						<li>
+							<Link to='/about-use-memo'>useMemo</Link>
+						</li>
+						<li>
+							<Link to='/about-use-callback'>useCallback</Link>
+						</li>
+						<li>
+							<Link to='/controlled-form'>controlled From</Link>
+						</li>
+						<li>
+							<Link to='/todo-list'>todoList</Link>
+						</li>
+					</ul>
+				</nav>
+				<button onClick={toggleTheme}>Toggle Theme</button>
+
+				<Routes>
+					<Route
+						path='/'
+						element={
+							<Greeting name={user.name} age={user.age} hobby={user.hobby} />
+						}
+					></Route>
+					<Route path='/about-use-state' element={<AboutUseState />}></Route>
+					<Route path='/about-use-effect' element={<AboutUseEffect />}></Route>
+					<Route path='/about-use-ref' element={<AboutUseRef />}></Route>
+					<Route path='/about-use-memo' element={<AboutUseMemo />}></Route>
+					<Route
+						path='/about-use-callback'
+						element={<AboutUseCallback />}
+					></Route>
+					<Route path='/controlled-form' element={<ControlledForm />}></Route>
+					<Route path='/todo-list' element={<TodoList />}></Route>
+				</Routes>
+			</div>
+		</Router>
 	)
 }
 
--- src/main.tsx
+++ src/main.tsx
@@ -2,7 +2,7 @@ import { StrictMode } from 'react'
 import { createRoot } from 'react-dom/client'
 import './index.css'
 import App from './App.tsx'
-import { ThemeProvider } from './hooks/about.useContext.tsx'
+import { ThemeProvider } from './about-hooks/about.useContext.tsx'
 createRoot(document.getElementById('root')!).render(
 	<StrictMode>
 		<ThemeProvider>
